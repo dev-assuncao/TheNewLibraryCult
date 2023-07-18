5e4d29880ddca4568c952910e05326a4ea53472c
dev-assuncao <davisson.assuncao@protonmail.com>
2023-07-17 22:21:50 -0300

    Feat: Custom response, method register and login implemented

--- LibraryCult/src/services/LibraryCult.Identity.API/Controllers/IdentityController.cs
+++ LibraryCult/src/services/LibraryCult.Identity.API/Controllers/IdentityController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LibraryCult.Identity.API.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using static LibraryCult.Identity.API.Models.UserViewModel;
 
 namespace LibraryCult.Identity.API.Controllers
@@ -7,16 +10,82 @@ namespace LibraryCult.Identity.API.Controllers
     [Route("api/[Controller]")]
     public class IdentityController : MainController
     {
-        //    [HttpGet]
-        //    public async Task<ActionResult<>> Login()
-        //    {
-        //    }
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly JWT _appSettings;
 
+        public IdentityController(SignInManager<IdentityUser> signInManager,
+                                  UserManager<IdentityUser> userManager,
+                                  IOptions<JWT> settings)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _appSettings = settings.Value;
+        }
 
         [HttpPost("register")]
-        public async Task<ActionResult<UserRegisterViewModel>> RegisterUser(UserRegisterViewModel register)
+        public async Task<ActionResult> RegisterUser(UserRegisterViewModel userRegister)
         {
-            return await;
+
+            if (!ModelState.IsValid)
+            {
+                return CustomResponse(ModelState);
+            }
+
+            var user = new IdentityUser
+            {
+                Email = userRegister.Email,
+                PasswordHash = userRegister.Password,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user);
+
+            if (result.Succeeded)
+            {
+                // chamar gerador de JWT
+                return Ok();
+            }
+
+            foreach (var erro in result.Errors)
+            {
+                AddProcessingError(erro.Description);
+            }
+
+            return CustomResponse();
         }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(UserLoginViewModel userLogin)
+        {
+            if (!ModelState.IsValid)
+            {
+                return CustomResponse(ModelState);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(userLogin.Email,
+                                                                  userLogin.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                //gerar JWT
+                return Ok();
+            }
+
+            if (result.IsLockedOut)
+            {
+                AddProcessingError("User is locked out by too many access wrong");
+                return CustomResponse();
+            }
+
+            AddProcessingError("User or Password is incorrect.");
+            return CustomResponse();
+        }
+
+
+        //public string Generate JsonWebToken()
+        //{
+
+        //}
     }
 }
--- LibraryCult/src/services/LibraryCult.Identity.API/Controllers/MainController.cs
+++ LibraryCult/src/services/LibraryCult.Identity.API/Controllers/MainController.cs
@@ -1,9 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Collections.Generic;
 
 namespace LibraryCult.Identity.API.Controllers
 {
     [ApiController]
-    public class MainController : ControllerBase
+    public abstract class MainController : ControllerBase
     {
+        protected ICollection<string> Errors = new List<string>();
+
+
+
+
+
+        protected ActionResult CustomResponse(object result = null)
+        {
+            if (ValidOperation()) return Ok(result);
+
+            return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+            {
+                {"Message", Errors.ToArray() }
+            }));
+
+        }
+
+        protected bool ValidOperation()
+        {
+            return !Errors.Any();
+        }
+
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+            var errors = modelState.Values.SelectMany(x => x.Errors).ToList();
+
+            foreach(var erro in errors)
+            {
+                AddProcessingError(erro.ErrorMessage);
+            }
+
+            return CustomResponse();
+        }
+
+        protected void AddProcessingError(string message)
+        {
+            Errors.Add(message);
+        }
+
+        protected void ClearErrorMessages()
+        {
+            Errors.Clear();
+        }
+        
     }
 }
